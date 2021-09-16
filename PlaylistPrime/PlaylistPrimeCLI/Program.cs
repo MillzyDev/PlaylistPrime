5fdeca04384c21ef756899feb362725380fbfd7d
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-09-16 07:11:52 +0100

    Created cli project

---
+++ PlaylistPrime/PlaylistPrimeCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaylistPrimeCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
